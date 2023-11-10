845a1560e03a5fea81fe89904dd37ca46fcf98a4
malewis5 <matthew.a.lewis7@gmail.com>
2023-11-09 21:11:22 -0500

    pretty cool

---
+++ src/components/spinner.tsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className='flex items-center justify-center'>
+      <div className='w-4 h-4 border-b-2 border-white rounded-full animate-spin'></div>
+    </div>
+  );
+};
+
+export default Spinner;
