157f4c8ab875a8f456bafd9ce5eeddbbaf8a02ef
malewis5 <matthew.a.lewis7@gmail.com>
2023-11-09 21:55:58 -0500

    add simple nav

---
+++ src/components/navbar.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
+import Link from 'next/link';
+
+export const Navbar = () => {
+  return (
+    <NavigationMenu className='absolute top-3 left-3'>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href='/' legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Utah
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href='/california' legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              California
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href='/tahoe' legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Tahoe
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
